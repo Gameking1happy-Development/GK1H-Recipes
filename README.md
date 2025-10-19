6251929bbfa6a63c27549495fb8c556c89765f01
Gameking1happy <gameking1happy@icloud.com>
2025-10-19 14:15:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Template for mod.  
+
+Discord: https://discord.gg/AwJvGzH
---
+++ fabric/src/main/java/io/github/gameking1happy/examplemod/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package io.github.gameking1happy.examplemod.platform;
+
+import io.github.gameking1happy.examplemod.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
